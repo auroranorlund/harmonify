ea6c941199b1502f9496126af09f8278c2b78475
Aurora Norlund <auroranorlund@gmail.com>
2025-11-26 18:53:41 -0700

    test code

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { redirectToAuthCodeFlow, getAccessToken, fetchProfile } from "./apiHandler.mjs";
 
-loadHeaderFooter();
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+
+async function home() {
+    if (!code) {
+        redirectToAuthCodeFlow(clientId);
+    } else {
+        const accessToken = await getAccessToken(clientId, code);
+        const profile = await fetchProfile(accessToken);
+        populateUI(profile);
+    }
+};
+
+home();
