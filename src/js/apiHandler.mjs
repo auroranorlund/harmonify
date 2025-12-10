b53299b0b0474da3c6b1122d52f3ce8b70589623
Aurora Norlund <auroranorlund@gmail.com>
2025-12-09 19:49:15 -0700

    test2

--- src/js/apiHandler.mjs
+++ src/js/apiHandler.mjs
@@ -68,5 +68,7 @@ export async function fetchProfile(token) {
     const result = await fetch("https://api.spotify.com/v1/me", {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
     });
-    return await result.json();
+    const response = await result.json();
+    console.log(response);
+    return response;
 }
